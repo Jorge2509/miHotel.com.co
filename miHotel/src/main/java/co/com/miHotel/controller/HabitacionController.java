1b596fafb2feb720b58b2e3411e47f79fecd2220
Ximena Valero <xvalerogouninpahu.edu.co>
2019-08-11 20:52:56 -0500

    11/08/2019 xv
    Librerias y programacion para FileUpload

--- miHotel/src/main/java/co/com/miHotel/controller/HabitacionController.java
+++ miHotel/src/main/java/co/com/miHotel/controller/HabitacionController.java
@@ -1,10 +1,5 @@
 package co.com.miHotel.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +8,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 import co.com.miHotel.dao.HabitacionDAO;
 import co.com.miHotel.dao.TipoHabitacionDAO;
@@ -34,6 +29,7 @@ public class HabitacionController {
 	private boolean isEdit = false;
 	private List<TipoHabitacion> tipoHabitacionList = new ArrayList<>();
 	private TipoHabitacionDAO tipoHabitacionDao = new TipoHabitacionDAO();
+	private UploadedFile file;
 
 	@PostConstruct
 	public void init() {
@@ -55,59 +51,129 @@ public class HabitacionController {
 		}
 	}
 
-	public void handleFileUpload(FileUploadEvent event) throws IOException {
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-		try {
-			if (event.getFile().getSize() <= 0) {
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						"Error:", "Ud. debe seleccionar un archivo de imagen \".png\""));
-				return;
-			}
+	public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+     
+    public void handleFileUpload(FileUploadEvent event) {
+    	System.out.println("Estoy aca llegue al metodoooo");
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 
-			if (!event.getFile().getFileName().endsWith(".png")) {
-				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						"Error:", "El archivo debe ser con extensión \".png\""));
-				return;
-			}
-
-			if (event.getFile().getSize() > 2097152) {
-				FacesContext.getCurrentInstance().addMessage(null,
-						new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", "El archivo no puede ser más de 2mb"));
-				return;
-			}
-
-			ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext()
-					.getContext();
-			String carpetaImagen = (String) servletContext.getRealPath("/imagen");
-			habitacion.setImagen(carpetaImagen + "/" + event.getFile().getFileName());
-			outputStream = new FileOutputStream(new File(carpetaImagen + "/" + "nombre"));
-			inputStream = event.getFile().getInputstream();
-
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			while ((read = inputStream.read(bytes)) != -1) {
-				outputStream.write(bytes, 0, read);
-			}
-
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "La imagen se cargo correctamente", "Correcto"));
-		} catch (
-
-		Exception ex) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,
-					"Error fatal:", "Por favor contacte con su administrador " + ex.getMessage()));
-		} finally {
-			if (inputStream != null) {
-				inputStream.close();
-			}
-
-			if (outputStream != null) {
-				outputStream.close();
-			}
-		}
-	}
+	// public void upload(FileUploadEvent event) {
+	// try {
+	// FacesMessage msg = new FacesMessage("Succesful",
+	// event.getFile().getFileName() + " is uploaded.");
+	// FacesContext.getCurrentInstance().addMessage(null, msg);
+	// copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+	// FacesMessage message = new FacesMessage("El archivo se ha subido con
+	// éxito!");
+	// FacesContext.getCurrentInstance().addMessage(null, message);
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// public void copyFile(String fileName, InputStream in) {
+	// try {
+	// String destination = "C:\\Users\\Ximena\\Pictures\\pruebas";
+	// OutputStream out = new FileOutputStream(new File(destination + fileName));
+	// int read = 0;
+	// byte[] bytes = new byte[1024];
+	// while ((read = in.read(bytes)) != -1) {
+	// out.write(bytes, 0, read);
+	// }
+	// in.close();
+	// out.flush();
+	// out.close();
+	// System.out.println("El archivo se ha creado con éxito!");
+	//
+	// DateFormat dateFormat = new SimpleDateFormat("yyyy–MM–dd HH_mm_ss");
+	// Date date = new Date();
+	// String ruta1 = destination + fileName;
+	// String ruta2 = destination + dateFormat.format(date) + "–" + fileName;
+	// System.out.println("Archivo: " + ruta1 + " Renombrado a: " + ruta2);
+	// File archivo = new File(ruta1);
+	// archivo.renameTo(new File(ruta2));
+	// } catch (IOException e) {
+	// System.out.println(e.getMessage());
+	// }
+	// }
+	//
+	// public void handleFileUpload(FileUploadEvent event) throws IOException {
+	// System.out.println("Estoy en el metodo de la imagen al fin :)");
+	// InputStream inputStream = null;
+	// OutputStream outputStream = null;
+	// try {
+	// if (event.getFile().getSize() <= 0) {
+	// FacesContext.getCurrentInstance().addMessage(null, new
+	// FacesMessage(FacesMessage.SEVERITY_ERROR,
+	// "Error:", "Ud. debe seleccionar un archivo de imagen \".png\""));
+	// return;
+	// }
+	//
+	// if (!event.getFile().getFileName().endsWith(".png")) {
+	// FacesContext.getCurrentInstance().addMessage(null, new
+	// FacesMessage(FacesMessage.SEVERITY_ERROR,
+	// "Error:", "El archivo debe ser con extensión \".png\""));
+	// return;
+	// }
+	//
+	// if (event.getFile().getSize() > 2097152) {
+	// FacesContext.getCurrentInstance().addMessage(null,
+	// new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", "El archivo no puede
+	// ser más de 2mb"));
+	// return;
+	// }
+	//
+	// ServletContext servletContext = (ServletContext)
+	// FacesContext.getCurrentInstance().getExternalContext()
+	// .getContext();
+	// String carpetaImagen = (String) servletContext.getRealPath("/imagen");
+	// habitacion.setImagen(carpetaImagen + "/" + event.getFile().getFileName());
+	// outputStream = new FileOutputStream(new File(carpetaImagen + "/" +
+	// "nombre"));
+	// inputStream = event.getFile().getInputstream();
+	//
+	// int read = 0;
+	// byte[] bytes = new byte[1024];
+	//
+	// while ((read = inputStream.read(bytes)) != -1) {
+	// outputStream.write(bytes, 0, read);
+	// }
+	//
+	// FacesContext.getCurrentInstance().addMessage(null,
+	// new FacesMessage(FacesMessage.SEVERITY_INFO, "La imagen se cargo
+	// correctamente", "Correcto"));
+	// } catch (
+	//
+	// Exception ex) {
+	// FacesContext.getCurrentInstance().addMessage(null, new
+	// FacesMessage(FacesMessage.SEVERITY_FATAL,
+	// "Error fatal:", "Por favor contacte con su administrador " +
+	// ex.getMessage()));
+	// } finally {
+	// if (inputStream != null) {
+	// inputStream.close();
+	// }
+	//
+	// if (outputStream != null) {
+	// outputStream.close();
+	// }
+	// }
+	// }
 
 	public void update(Integer id) {
 		try {
